f205c817ad1cb63f8b41705db4f013f4b82f8e31
bzcode-todox <dangdinhphong@gmail.com>
2026-03-30 18:52:53 +0700

    Fix: Define phong in vite config for Render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,6 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// Thiết lập __dirname cho ES Modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
